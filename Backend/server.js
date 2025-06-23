f60d3f857eb6c0d0b0956c447bfb515b1da9a339
Arnab <arnavmallick12006@gmail.com>
2025-06-24 02:32:37 +0530

    fr bug

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors()); // comment 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
